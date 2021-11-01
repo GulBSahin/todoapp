5370ece495977f45bb100570cca6861699b0d404
GulBSahin <70208660+GulBSahin@users.noreply.github.com>
2021-11-01 10:42:08 +0300

    updated css

--- app.js
+++ app.js
@@ -1,89 +1,57 @@
 //Variables
 const ul=document.querySelector(".todolist-list-container"); /* ul listesi*/
-const button=document.querySelector(".add-btn");
-const inputText=document.querySelector("#task");
-const form=document.querySelector("#add-task");
+const button=document.querySelector(".add-btn"); //ekleme butonu
+const inputText=document.querySelector("#task"); //inputText
+const form=document.querySelector("#add-task"); //form elemanı 
 const deleteButton=document.querySelectorAll(".btn"); /* bütün butonları döktü */
-const hidenDiv=document.querySelector("#footer");
-
-console.log(ul);
-console.log(button);
-console.log(inputText);
-
 //Functions
 const createTask=(taskText)=>{    
-    return `
+    return ` 
     <div class="list-group">
                     <li class="unchecked">
                         ${taskText}
                     </li>
-                    <button class="btn"><i class="fas fa-trash-alt"></i></button>
-                </div>
+                    <button class="btn">&#128473;</button>
+                </div> 
     `
 }
-
+//this function calculates the Completed tasks out of all tasks
+function completedTaskCount(){
+    checkedCount = ul.getElementsByClassName("checked").length; //length the completed tasks
+    taskCount = ul.getElementsByClassName("list-group").length; //length the all tasks
+       
+    const  checkedTasks=document.getElementById("checkedTasks"); //p class completed task count 
+    const  allTasks=document.getElementById("allTasks"); //p class all task count 
+      
+    checkedTasks.innerHTML=checkedCount; //send the value to html p element
+    allTasks.innerHTML=taskCount ; //send the value to html p element
+}
 //Events
 //add task event
-button.addEventListener("click", e=>{
-    e.preventDefault(); 
-    ul.innerHTML += createTask(inputText.value);
-    form.reset()
-     //completed task count 
-     var checkedCount = ul.getElementsByClassName("checked").length;
-     console.log(checkedCount);
-     var taskCount = ul.getElementsByClassName("list-group").length;
-     console.log(taskCount);
-     
-     const  checkedTasks=document.getElementById("checkedTasks"); 
-     const  allTasks=document.getElementById("allTasks"); 
-       
-     checkedTasks.innerHTML=checkedCount;
-     allTasks.innerHTML=taskCount ;
-   
+button.addEventListener("click", e=>{  //add event listening to add buton
+    e.preventDefault();  //to prevent reload page after each click
+    if(inputText.value!==""){ //if input text box is not empty go alert
+    ul.innerHTML += createTask(inputText.value); //get input value create a text and put it ul as li 
+    form.reset() //reset input area
+    }else{
+        alert("please enter a task"); //if input box is not empty go alert
+    }
+    completedTaskCount() 
 })
-
 //remove task event
-ul.addEventListener("click", e=> {
-    if(e.target.classList.contains("btn")) {
-        e.target.parentElement.remove();
+ul.addEventListener("click", e=> {    //add event listener to ul for delete buttons to remove elements
+    if(e.target.classList.contains("btn")) { //if we target the element that have btn class  
+        e.target.parentElement.remove(); //remove their parent elements
     }   
-console.log(e.target.classList.contains("btn"));
-//completed task count 
-checkedCount = ul.getElementsByClassName("checked").length;
-console.log(checkedCount);
-taskCount = ul.getElementsByClassName("list-group").length;
-console.log(taskCount);
-
-const  checkedTasks=document.getElementById("checkedTasks"); 
-const  allTasks=document.getElementById("allTasks"); 
-  
-checkedTasks.innerHTML=checkedCount;
-allTasks.innerHTML=taskCount ;
-console.log(ul);
+    completedTaskCount()
 })
-
-//check the items events
-ul.addEventListener("click", e=> {
-    if(e.target.classList.contains("unchecked")) {
-        e.target.closest("li").className = "checked";
+//check or unchecked the items events
+ul.addEventListener("click", e=> {  //add evenet listener to ul for changing check status
+    if(e.target.classList.contains("unchecked")) { //if we target the element that have unchecked class name
+        e.target.closest("li").className = "checked"; //change its class name to checked
     }else  {
-        e.target.closest("li").className = "unchecked";
+        e.target.closest("li").className = "unchecked"; //if its name checked, change it as unchecked
     } 
-//  console.log(e.target.classList.contains("unchecked"));
- console.log(e.target);
- 
- //completed task count 
- checkedCount = ul.getElementsByClassName("checked").length;
- console.log(checkedCount);
- taskCount = ul.getElementsByClassName("list-group").length;
- console.log(taskCount);
- 
- const  checkedTasks=document.getElementById("checkedTasks"); 
- const  allTasks=document.getElementById("allTasks"); 
-   
- checkedTasks.innerHTML=checkedCount;
- allTasks.innerHTML=taskCount ;
- console.log(ul);
-
+    completedTaskCount()
 })
 
